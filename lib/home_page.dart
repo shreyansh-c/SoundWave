a46f6981a8ffbe1232ada8e29f6aef68325b0d06
shreyansh-c <shreyanshchaudhary4766@gmail.com>
2023-07-11 22:52:35 +0530

    Homescreen Completed

---
+++ lib/home_page.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:sound_wave/feature_box.dart';
+import 'package:sound_wave/pallete.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SoundWave'),
+        centerTitle: true,
+        leading: const Icon(Icons.menu),
+      ),
+      body: Column(
+        children: [
+          //prof
+          Stack(
+            children: [
+              Center(
+                child: Container(
+                  height: 120,
+                  width: 120,
+                  margin: const EdgeInsets.only(top: 4),
+                  decoration: const BoxDecoration(
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle),
+                ),
+              ),
+              Container(
+                height: 100,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/robot2.png')),
+                ),
+              )
+            ],
+          ),
+          //chatbub
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
+              top: 30,
+            ),
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: Pallete.borderColor,
+                ),
+                borderRadius:
+                    BorderRadius.circular(20).copyWith(topLeft: Radius.zero)),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Good morning what task can i do for you?',
+                style: TextStyle(
+                    color: Pallete.mainFontColor,
+                    fontFamily: 'Cera Pro',
+                    fontSize: 25),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            alignment: Alignment.centerLeft,
+            margin: const EdgeInsets.only(top: 10, left: 22),
+            child: const Text(
+              'Here are a few Features',
+              style: TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.mainFontColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          //featureslist
+          Column(
+            children: const [
+              FeatureBox(
+                color: Pallete.firstSuggestionBoxColor,
+                headerText: 'ChatGPT',
+                descriptionText:
+                    'A smarter way to stay organised and informed with ChatGPT',
+              ),
+              FeatureBox(
+                color: Pallete.secondSuggestionBoxColor,
+                headerText: 'Dall-E',
+                descriptionText:
+                    'Stay inspired and stay creative with your personal assistant powered by Dall-E',
+              ),
+              FeatureBox(
+                  color: Pallete.thirdSuggestionBoxColor,
+                  headerText: 'Smart Voice Assistant',
+                  descriptionText:
+                      'Get the best of both worlds with a voice assistant powered by ChatGPT and Dall-E'),
+            ],
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Pallete.firstSuggestionBoxColor,
+        onPressed: () {},
+        child: const Icon(Icons.mic),
+      ),
+    );
+  }
+}
